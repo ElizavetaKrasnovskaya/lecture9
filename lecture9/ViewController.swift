34c40157c2a7432d67d6837f9b638d7b99fb3c71
Elizaveta Krasnovskaya <y.krasnouskaya>
2022-09-22 22:27:33 +0200

    Initial Commit

---
+++ lecture9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lecture9
+//
+//  Created by admin on 22/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
